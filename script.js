2e0d50b652c22bf40d01a0034066ec53d485b77a
Ahmedryhany <74978357+Ahmedryhany@users.noreply.github.com>
2024-11-02 18:30:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<section id="about">
+    <h2>من أنا</h2>
+    <p>أنا العبد لله الذي مسكَ الحديدَ تناه</p>
+    <button onclick="toggleMoreInfo()">عرض المزيد</button>
+    <div id="more-info" style="display: none;">
+        <p>pla pla pla</p>
+    </div>
+</section>
